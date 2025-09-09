a03c3ca17355df03a878c75e80005cf4bd4a76bb
Pradeep tanwar <pradeeptanwar166@gmail.com>
2025-09-09 18:51:51 +0530

    Update Pages.jsx
    updated code

--- src/Components/Pages.jsx
+++ src/Components/Pages.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Pages() {
   return (
-    <div>Pages</div>
+    <div>
+      <div className='flex justify-around items-center bg-purple-600'>
+      <div><img src="https://rukminim2.flixcart.com/fk-p-flap/64/64/image/5f2ee7f883cdb774.png?q=100" /><a className='hover:text-white' href=""> Mobiles & Tablets</a></div>
+      <div><img src="https://rukminim2.flixcart.com/fk-p-flap/64/64/image/af646c36d74c4be9.png?q=100" /><a className='hover:text-white' href="">Laptops</a></div>
+      <div> <img src="https://rukminim2.flixcart.com/fk-p-flap/64/64/image/e730a834ad950bae.png?q=100"/><a className='hover:text-white' href="">Grocery</a></div>
+      <div><img src="https://rukminim2.flixcart.com/fk-p-flap/64/64/image/ff559cb9d803d424.png?q=100" /><a className='hover:text-white' href="">Fashion</a></div>
+      <div> <img src="https://rukminim2.flixcart.com/fk-p-flap/64/64/image/b3020c99672953b9.png?q=100"/><a className='hover:text-white' href="">Home Decor</a></div>
+      <div> <img src="https://rukminim2.flixcart.com/fk-p-flap/64/64/image/1788f177649e6991.png?q=100" /><a className='hover:text-white' href="">Furniture</a></div></div>
+    </div>
   )
 }
 

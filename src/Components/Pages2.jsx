42c8f16ef3b48c3d271ccc8df10e45a385ea4f6f
Pradeep tanwar <pradeeptanwar166@gmail.com>
2025-09-17 18:08:55 +0530

    small updates

--- src/Components/Pages2.jsx
+++ src/Components/Pages2.jsx
@@ -4,43 +4,43 @@ function Pages2() {
   return (
     <div>
         <div className='grid grid-cols-4 m-4'>
-  <div className=' h-96 border-none bg-white/90 rounded-md m-2'>
+  <div className=' min-h-96 border-none bg-white/90 rounded-md m-2'>
   <h1 className='text-2xl flex text-center justify-center mt-4 font-semibold'>Coustomer's most demanded shoes</h1>
   <div className='grid grid-cols-2 mt-3'>
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/51-CObvtVsL._AC_SY145_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/71UZLYpHA9L._AC_UL480_FMwebp_QL65_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/71YVs0RXcFL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/61VHvg7wvCL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer  object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/51-CObvtVsL._AC_SY145_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer  object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/71UZLYpHA9L._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer  object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/71YVs0RXcFL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer  object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/61VHvg7wvCL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
   </div>
   <a className='m-5 hover:text-blue-400 ' href="#">explore more <ChevronRightIcon></ChevronRightIcon></a>
   </div>
-  <div className=' h-96 border-none bg-white/90 rounded-md m-2'>
+  <div className=' min-h-96 border-none bg-white/90 rounded-md m-2'>
   <h1 className='text-2xl flex text-center justify-center font-semibold mt-4'>Most searched items on indian mart</h1>
   <div className='grid grid-cols-2 mt-3'>
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_2._SY116_CB567468236_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_3._SY116_CB567468236_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_4._SY116_CB567468236_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_1._SY116_CB567468236_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_2._SY116_CB567468236_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_3._SY116_CB567468236_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_4._SY116_CB567468236_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/PB/March/Bikram/PC_QC_HOME_SIZE_186_1._SY116_CB567468236_.jpg" alt="" />
   </div>
   <a className='m-5 hover:text-blue-400 ' href="#">explore more <ChevronRightIcon></ChevronRightIcon></a>
   </div>
-  <div className=' h-96 border-none bg-white/90 rounded-md m-2'>
+  <div className=' min-h-96 border-none bg-white/90 rounded-md m-2'>
   <h1 className='text-2xl flex text-center justify-center font-semibold mt-4'>Appliances for your Home </h1>
   <div className='grid grid-cols-2 mt-3'>
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08345R1ZW._SY116_CB667322346_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B07G5J5FYP._SY116_CB667322346_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/186x116---wm._SY116_CB667322346_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08345R1ZW._SY116_CB667322346_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B07G5J5FYP._SY116_CB667322346_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/186x116---wm._SY116_CB667322346_.jpg" alt="" />
   </div>
   <a className='m-5 hover:text-blue-400 ' href="#">explore more <ChevronRightIcon></ChevronRightIcon></a>
   </div>
-  <div className=' h-96 border-none bg-white/90 rounded-md m-2'>
+  <div className=' min-h-96 border-none bg-white/90 rounded-md m-2'>
   <h1 className='text-2xl flex text-center justify-center mt-4 font-semibold'>Toys for your kids</h1>
   <div className='grid grid-cols-2 mt-3'>
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/51pGit1uTFL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/51yqopIqHrL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/61JOvhicnHL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
-  <img className='w-32 m-2 cursor-pointer' src="https://m.media-amazon.com/images/I/41UbqtnGdVL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/51pGit1uTFL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/51yqopIqHrL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/61JOvhicnHL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+  <img className='w-32 m-2 cursor-pointer object-fill overflow-hidden' src="https://m.media-amazon.com/images/I/41UbqtnGdVL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
   </div>
   <a className='m-5 hover:text-blue-400 ' href="#">explore more <ChevronRightIcon></ChevronRightIcon></a>
   </div>
